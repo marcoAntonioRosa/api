cb835c01839ceda10a61d15cde3c9b6905437be1
Marco Antonio Rosa da Silva <marco.silva@unochapeco.edu.br>
2020-07-16 18:00:35 -0300

    Encriptação de senha adicionada

--- server/src/controllers/loginController.js
+++ server/src/controllers/loginController.js
@@ -1,44 +1,48 @@
 const Login = require('../models/Login')
+const bCrypt = require('bcrypt')
 
 exports.get = (req, res) => {
     let id = req.params.id;
 
     Login.findOne({ where: {id: id}}).then(response => {
-        console.log('Exibindo um usuário')
         res.status(200).json(JSON.parse(JSON.stringify(response)));
     })
 }
 
 exports.getAll = (req, res) => {
     Login.findAll().then(response => {
-        console.log('Exibindo todos os usuários')
         res.status(200).json(JSON.parse(JSON.stringify(response)));
     });
 }
 
 exports.post = (req, res) => {
-    let usuario = req.body.usuario;
-    let senha = req.body.senha;
+    bCrypt.hash(req.body.senha, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).json({
+                error: err
+            })
+        } else {
+            let data = {
+                usuario: req.body.usuario,
+                senha: hash
+            }
 
-    let data = {
-        usuario: usuario,
-        senha: senha,
-    };
-
-    Login.create(data).then(response => {
-        console.log('Usuário criado')
-        res.status(200).json(response);
+            Login.create(data).then(response => {
+                res.status(201).json(response);
+            }).catch((err) => {
+                res.status(500).json({
+                    error: err
+                });
+            })
+        }
     })
 }
 
 exports.update = (req, res) => {
     let id = req.body.id;
-    let usuario = req.body.usuario;
-    let senha = req.body.senha;
-
     let data = {
-        usuario: usuario,
-        senha: senha,
+        usuario: req.body.usuario,
+        senha: req.body.senha
     };
 
     Login.update(data, {
