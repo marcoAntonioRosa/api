516a48acc4dd97811755da247ad6aec5163a95e8
Marco Antonio Rosa da Silva <marco.silva@unochapeco.edu.br>
2020-07-23 14:40:47 -0300

    Remoção de dependência redundânte. Body-parser ja está incluso em no módulo express, por isso não sua inclusão manual não é necessaria. package.json e package-lock.json foram movidos para dentro da pasta server

--- server/app.js
+++ server/app.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 
 const usuario = require('./src/routes/usuarioRoutes')
 const checkAuth = require('./src/middleware/checkAuth')
+
+app.use(express.json())
 
 app.get('/', checkAuth, (req, res) => {
     res.send('Hello World')
 })
 
-app.use(bodyParser.json())
-
 app.use('/usuario', usuario)
-
 
 module.exports = app
